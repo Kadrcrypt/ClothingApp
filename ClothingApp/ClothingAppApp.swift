6651560e048941ca1d3dc40a2f4bf9d0a022fc98
m0ln1z <anartur476@gmail.com>
2024-09-25 23:40:57 +0500

    Initial Commit

---
+++ ClothingApp/ClothingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClothingAppApp.swift
+//  ClothingApp
+//
+//  Created by Артур Ан on 25.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct ClothingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClothingAppTests/ClothingAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClothingAppTests.swift
+//  ClothingAppTests
+//
+//  Created by Артур Ан on 25.09.2024.
+//
+
+import Testing
+@testable import ClothingApp
+
+struct ClothingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
